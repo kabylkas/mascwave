126a5cad7ff3ab514f4836776a9e7490bd0baacf
root <root@ip-172-31-6-212.us-west-1.compute.internal>
2018-01-20 06:50:34 +0000

    Final Draft

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const io = require('socket.io')(http,{
 	cookie: true
 });
+const debug = false;
 
 var file = {
     name: "",
@@ -39,15 +40,20 @@ app.post('/upload', function(req, res) {
 		if(req.body){
 			var t;
 			var tighttext = req.body.tight_form_text;
-			console.log(tighttext);
-			try {t = eval('(function(){'+tighttext+'}())');} catch(err){return res.status(400).send(err);}
-			var tight_json = generate_tight(t);
+			var lines = tighttext.split('\n');
+			var u = '';
+			for (s = 0; s < lines.length; s++){
+				
+				try {t = eval('(function(){'+lines[s]+'}())'); u = u + t + '\n';} catch(err){return res.status(400).send(err);}
+			}
+			
+			var tight_json = split_lines(u);
 			fs.writeFile("tmp/"+req.body.tight_form_id, tight_json, function(err) {
 				if(err) {
 					return console.log(err);
 				}
 
-				console.log("The file "+req.body.tight_form_id+" was saved!");
+				if(debug) console.log("The file "+req.body.tight_form_id+" was saved!");
 			}); 
 		} else 
 			return res.status(400).send('No files were uploaded nor any input was given.');
@@ -55,10 +61,10 @@ app.post('/upload', function(req, res) {
 		// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
 		//console.log(req.body.fileName);
 		let sampleFile = req.files.sampleFile;
-		//console.log(sampleFile);
+		if(debug) console.log(sampleFile);
 		
 
-		console.log("file uploaded: "+ sampleFile);
+		if(debug) console.log("file uploaded: "+ sampleFile.name);
 		// Use the mv() method to place the file somewhere on your server
 		var newFileName = 'tmp/'+req.body.fileName;//req.files.sampleFile.name;
 		sampleFile.mv(newFileName, function(err) {
@@ -91,9 +97,9 @@ io.on('connection',function(socket){
 //On client sent data
 	socket.on('clientdata', function(data){
 		console.log(data.filename+"\n"+data.socketid+": \n start time:"+data.start_time+"\n window size:"+data.window_size+ "\n element start: "+data.v_start_time+"\n element end:"+data.v_size);
-		if(k){ //Makes sure k is defined
+		if(k != undefined){ //Makes sure k is defined
 			data.server_source.signal = k.signal.slice(data.v_start_time,data.v_size);
-			console.log("Source" + data.server_source);
+			if(debug) console.log("Source" + data.server_source);
 		}
 		var a = 0;
 		//if(file.eva != null)
@@ -116,9 +122,9 @@ io.on('connection',function(socket){
 					
 				}
 				userdata.source_size = k.signal.length;
-				console.log("size: "+ userdata.source_size);
+				if(debug) console.log("size: "+ userdata.source_size);
 				userdata.server_source.signal = k.signal.slice(userdata.v_start_time,userdata.v_size);
-				console.log("Chunk of data \n" + JSON.stringify(userdata.server_source.signal,null,4));
+				if(debug) console.log("Chunk of data \n" + JSON.stringify(userdata.server_source.signal,null,4));
 				socket.emit('serverdata', userdata);
 			}
 		});
@@ -136,25 +142,41 @@ var server = http.listen(5901, function(){
     console.log('Listening on port %d', server.address().port);
 });
 
-function generate_tight(array){
+function split_lines(examples){
+	var coordinates = examples.split( "\n" );
+	var final_string = [];
+	final_string += '{ signal : ['
+	for( var i = 0; i < coordinates.length -1; ++i ) {
+		var res = generate_tight(coordinates[i],i);
+		if(i != 0)
+			final_string += ',';
+		final_string += res;
+		
+	}
+	final_string += ']}';
+	return final_string;
+	//console.log(final_string);
+}
+
+function generate_tight(array,ind){
 	if (array != null || array != '' || !array.includes('\n')){
 		var obj = require('csv-string'),
 		arr = obj.parse(array);
-		console.log(arr);
+		if(debug) console.log(arr);
 		//{signal:[{ name: 'I-18', wave: '=...=', node: '\u0012', data: [ 'iAALU' ] }]}
-		var result = '{ signal : [{name: "I-1", wave:\'';
+		var result = '{name: "I-'+ind.toString(16)+'", wave:\'';
 		for (p =0 ; p < arr[0].length;p++){
 			result += '=';
 		}
-		result += '\', node: \'\\u0001\', data:[';
+		result += '\', node: \'\\u000'+ind.toString(16)+'\', data:[';
 		for (p =0 ; p < arr[0].length;p++){
 			if(p !=0){
 				result += ',';
 			}
 			result += '\''+arr[0][p]+'\'';
 		}
-		result += '] }]}';
-		console.log(result);
+		result += '] }';
+		if(debug) console.log(result);
 	}
 	return result;
 }
